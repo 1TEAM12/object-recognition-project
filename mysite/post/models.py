f46122f566a9e2b5054943e78c40fe31b84a8769
ssa <wogur981208@gmail.com>
2022-10-17 19:40:45 +0900

    Feat: Post model, admin추가
    - 1. 수정 post/models.py, post/admin.py
    - 2. model에는 Post라는 것을 추가하고 admin에는 Post가 보여지도록 추가함

--- mysite/post/models.py
+++ mysite/post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to="post_pics")
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    
+    def __str__(self):
+        return self.content
