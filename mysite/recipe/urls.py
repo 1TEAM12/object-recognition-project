9bb3b6581217ba4686e7457f936022ad1c77b145
ssa <wogur981208@gmail.com>
2022-10-18 20:43:36 +0900

    Feat : recipe url 설정

---
+++ mysite/recipe/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('recipe/<int:recipe_id>', views.recipe_detail, name='recipe-detail'),
+    path('recipe/like/list/<int:recipe_id>', views.recipe_like_list, name='recipe-like-list'),
+]
