75ecf5643054e1b3d65a403b966c785fbffe1203
ssa <wogur981208@gmail.com>
2022-10-18 20:38:28 +0900

    Feat : Recipe 모델 추가하고 어드민에 추가

--- mysite/recipe/models.py
+++ mysite/recipe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user.models import User
+from post.models import Post
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=30, blank=True)
+    image = models.ImageField(upload_to="post_pics", blank=True)
+    youtube_url = models.URLField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    like_authors = models.ManyToManyField(User, related_name='like_recipes')
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='recipes')
+    
+    def __str__(self):
+        return self.title
