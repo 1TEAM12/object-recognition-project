d8957dea377ddbd1a207c20fcf87ee6ac2357ef2
ssa <wogur981208@gmail.com>
2022-10-19 21:29:42 +0900

    Feat : user 팔로우 모델 수정

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
 # Create your models here.
 
 class User(AbstractUser):
     username = models.CharField(max_length=10, unique=True, null=True)
     email = models.CharField(max_length=50, unique=True, null=True)
-    follow = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='followee')
+    followings = models.ManyToManyField('self', symmetrical=False, blank=True, related_name= 'followers')
