07f53acafd4eea52e12f14c7c65a03a9ed87b592
JayJin <jvidorca@gmail.com>
2022-10-20 11:41:29 +0900

    Feat : Post, Recipe 모델 수정
    -1. post/models.py, recipe/models.py
    -2. img_detection기능 구현을 위한 모델 컬럼 수정

--- mysite/post/img_det.py
+++ mysite/post/img_det.py
@@ -9,11 +9,12 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path='model_det/best.pt',
 fruit_model = ['apple', 'banana', 'pineapple', 'orange', 'pear', 'guava' ]
 
 def pick_img(img_url):
-    img = cv2.imread(img_url)    # 이미지 불러오기
-    results = model(img)                                # 이미지 검사
-    img_detected = list()                # 이미지 검사 후 저장할 리스트 지정
-    results.save()
+    img = cv2.imread(img_url)            # load image file root
+    results = model(img)                 # detecting image file
+    results.save()                       # save the results
     result = results.pandas().xyxy[0].to_numpy()
+    
+    img_detected = list()                # create list for saving ingredients in image
 
     for item in result:
         try:
@@ -22,6 +23,6 @@ def pick_img(img_url):
         except:
             return redirect('post:post-create-img',{'error':'재료를 찾을 수 없습니다.'})
         
-    picked = random.choice(img_detected)            # 추출한 과일이름 중 하나 선택
+    picked = random.choice(img_detected)            # choice the ingredient
     return redirect('post:post-create',picked=picked)
 
--- mysite/recipe/models.py
+++ mysite/recipe/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from user.models import User
-from post.models import Post
+from post.models import Post, Dessert
 
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=30, blank=True)
-    image = models.ImageField(upload_to="recipe_pics", blank=True)
     youtube_url = models.URLField(max_length=100)
+    ingred = models.CharField(max_length=20, blank=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    
+      
     like_authors = models.ManyToManyField(User, related_name='like_recipes')
-
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, related_name='recipes')
     
     def __str__(self):
         return self.title
